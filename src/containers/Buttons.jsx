fd21aa341bb0ee955784ed2f9f9597944d18f3b5
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-10-07 02:27:40 +0000

    buttons

---
+++ src/containers/Buttons.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { fetchCryptosRequest } from "../redux/cryptos/cryptos.actions";
+import { selectNext, selectPrevious } from "../redux/cryptos/cryptos.selectors";
+import { createStructuredSelector } from "reselect";
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledButton = styled.button`
+  min-width: 4rem;
+  border: 1px solid rgba(7, 7, 7, 0.5);
+  box-shadow: 2px 1px grey;
+  cursor: ${props => (props.disabled ? "not-allowed" : "pointer")};
+`;
+
+const Buttons = ({ fetchCryptos, next, previous }) => {
+  const fetchNext = () => {
+    fetchCryptos(next);
+  };
+  const fetchPrevious = () => {
+    fetchCryptos(previous);
+  };
+  return (
+    <StyledContainer>
+      <StyledButton disabled={previous !== null} onClick={fetchPrevious}>
+        Previous
+      </StyledButton>
+      <StyledButton disabled={next !== null} onClick={fetchNext}>
+        Next
+      </StyledButton>
+    </StyledContainer>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  next: selectNext,
+  previous: selectPrevious
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchCryptos: start => dispatch(fetchCryptosRequest(start))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Buttons);
