ea1c40d0bc3bf3842af1458f255827ee70508150
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-10-07 00:30:10 +0000

    create selectors

---
+++ src/redux/cryptos/cryptos.selectors.js
@@ -0,0 +1,28 @@
+import { createSelector } from "reselect";
+
+const selectState = state => state;
+
+export const selectError = createSelector(
+  [selectState],
+  state => state.error
+);
+
+export const selectCryptors = createSelector(
+  [selectState],
+  state => state.cryptos
+);
+
+export const selectPrevious = createSelector(
+  [selectState],
+  state => state.previous
+);
+
+export const selectNext = createSelector(
+  [selectState],
+  state => state.next
+);
+
+export const selectLoading = createSelector(
+  [selectState],
+  state => state.isFetching
+);
